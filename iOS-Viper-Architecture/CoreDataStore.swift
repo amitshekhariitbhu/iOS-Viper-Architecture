119b44cfb12be4f129c67beb4738bde796f677f8
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2017-02-18 19:48:55 +0530

    Add CoreDataStore

---
+++ iOS-Viper-Architecture/CoreDataStore.swift
@@ -0,0 +1,35 @@
+//
+//  CoreDataStore.swift
+//  iOS-Viper-Architecture
+//
+//  Created by Amit Shekhar on 18/02/17.
+//  Copyright © 2017 Mindorks NextGen Private Limited. All rights reserved.
+//
+
+import CoreData
+import UIKit
+
+class CoreDataStore {
+    
+    static var persistentStoreCoordinator: NSPersistentStoreCoordinator? {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            return appDelegate.persistentContainer.persistentStoreCoordinator
+        }
+        return nil
+    }
+    
+    static var managedObjectModel: NSManagedObjectModel? {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            return appDelegate.persistentContainer.managedObjectModel
+        }
+        return nil
+    }
+    
+    static var managedObjectContext: NSManagedObjectContext? {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            return appDelegate.persistentContainer.viewContext
+        }
+        return nil
+    }
+    
+}
