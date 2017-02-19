06323ac7264d48538cd46ce3babaf0315e42bccf
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2017-02-19 23:14:46 +0530

    Change allignments in storyboard

--- iOS-Viper-Architecture/PostList/View/PostTableViewCell.swift
+++ iOS-Viper-Architecture/PostList/View/PostTableViewCell.swift
@@ -11,12 +11,12 @@ import AlamofireImage
 
 class PostTableViewCell: UITableViewCell {
 
-    
     @IBOutlet weak var postImageView: UIImageView!
     
     @IBOutlet weak var titleLabel: UILabel!
     
     func set(forPost post: PostModel) {
+        self.selectionStyle = .none
         titleLabel?.text = post.title
         let url = URL(string: post.thumbImageUrl)!
         let placeholderImage = UIImage(named: "placeholder")!
